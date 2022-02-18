b53cb2c1cddcf7825dd08bc5a06292488ddf3174
Dmitry <taldarim3@gmail.com>
2022-02-18 20:56:34 +0300

    Added DAL. Entities created and described

---
+++ Akvelon.TaskTracker.DataAccessLayer/Entities/Task.cs
@@ -0,0 +1,22 @@
+﻿namespace Akvelon.TaskTracker.DataAccessLayer.Entities;
+
+/// <summary>
+/// This class defines the entity of Task
+/// </summary>
+public class Task
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+    
+    public string Description { get; set; }
+    
+    public TaskStatus Status { get; set; }
+
+    public int Priority { get; set; }
+    
+    public int ProjectId { get; set; }
+        
+    public Project Project { get; set; }
+    
+}
