b11a8d394ad9d798721b0d90e26cffa2a64afb3d
Dmitry <taldarim3@gmail.com>
2022-02-26 13:26:03 +0300

    Added interfaces for filtering and sorting services. Interfaces are implemented.

---
+++ Akvelon.TaskTracker.BusinessLogicLayer/Services/Interfaces/IFilteringSevice.cs
@@ -0,0 +1,15 @@
+﻿using Akvelon.TaskTracker.DataAccessLayer.Entities;
+using Akvelon.TaskTracker.DataAccessLayer.Enums;
+
+namespace Akvelon.TaskTracker.BusinessLogicLayer.Services.Interfaces;
+
+public interface IFilteringSevice
+{
+    public IList<Project> FilteringByStartDateAfter(IList<Project> projects, DateTime dateAfter);
+        
+    public IList<Project> FilteringByEndDateBefore(IList<Project> projects, DateTime dateBefore);
+        
+    public IList<Project> FilteringCountOfTasksInRange(IList<Project> projects, int start, int end);
+
+    public IList<Project> FilteringByProjectStatus(IList<Project> projects, ProjectStatus status);
+}
---
+++ Akvelon.TaskTracker.BusinessLogicLayer/Services/Interfaces/ISortingService.cs
@@ -0,0 +1,15 @@
+﻿using Akvelon.TaskTracker.DataAccessLayer.Entities;
+
+namespace Akvelon.TaskTracker.BusinessLogicLayer.Services.Interfaces;
+
+public interface ISortingService
+{
+    public IList<Project> SortByStartDateDescending(IList<Project> projects);
+        
+    public IList<Project> SortByCompletionDateAscending(IList<Project> projects);
+        
+    public IList<Project> SortByPriorityDescending(IList<Project> projects);
+
+    public IList<Project> SortByStatusAscending(IList<Project> projects);
+
+}
